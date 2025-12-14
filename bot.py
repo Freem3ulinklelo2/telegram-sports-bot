f66cd631ff706e51cc2a400c6ceba39df2c9cafa
Freem3ulinklelo2 <Livetoons05@gmail.com>
2025-12-14 16:49:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ from telegram import Bot
 import logging
 import os
 
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
-GROUP_ID = int(os.environ.get('GROUP_ID'))
+BOT_TOKEN = os.environ.get('8274488764:AAHfo87eHioo2Iz_Ii2kyVIPHDcIuc6hnRo')
+GROUP_ID = int(os.environ.get('-1005019746478'))
 
 SONY = "https://raw.githubusercontent.com/drmlive/sliv-live-events/main/sonyliv.json"
 FANCODE = "https://raw.githubusercontent.com/drmlive/fancode-live-events/main/fancode.json"
